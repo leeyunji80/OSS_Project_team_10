f8f8b66bbcf081e2ccb64f2de38b3ad3e0c96dfa
leeyunji80 <leeyunji80@gmail.com>
2025-10-22 18:05:41 +0900

    Update main.c

--- main.c
+++ main.c
@@ -19,24 +19,21 @@ int Findlcm(int a, int b) {
 }
 
 int main(){
-  int n1 = 0, n2 = 0;
-  int gcd = 0, lcm = 0;
+  int num1, num2 = 0;
 
-  printf("Enter two integers>>");
-  scanf("%d%d", &n1, &n2);
+  printf("두 정수 입력>>");
+  scanf("%d %d", &num1, &num2);
 
-  if(n2 > n1){
-    int tmp = n1;
-    n1 = n2;
-    n2 = tmp;
+  if(num1 == 0 && num2 == 0){
+    return 0;
   }
 
-  printf("\nPrime numbers>>");
-  PrintPrime(n2);
-  gcd = FindGcd(n1, n2);
-  lcm = Findlcm(n1, n2);
+  findPrimes(num1, num2);
 
-  printf("\nGCD = %d, LCM = %d", gcd, lcm);
+  int gcd = calculateGCD(num1, num2);
+  int lcm = Findlcm(num1, num2, gcd);
+
+  printf("GCD = %d  LCM = %d \n", gcd, lcm);
 
   return 0;
 }
